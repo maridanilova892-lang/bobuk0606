3b20578c1306f2149f87d59409ad8ea1bdb4d9bd
bobuk0606 <maridanilova892@gmail.com>
2026-04-03 20:12:33 +0900

    Update code_checker.py
    password-Python1989

--- code_checker.py
+++ code_checker.py
@@ -1,20 +1,20 @@
 # put your python code here
 print("Welcome! enter a secret code to access log in to your account")
 password="Python1989"
-попытки=3
-счет=1
+attempts=3
+count=1
 while True:
     a=input()
     if a!=password:
-        попытки-=1
-        if попытки>0:
+        attempts-=1
+        if attempts>0:
             
-            print("Wrong answer! You have", попытки ,"attempts left")
-            счет+=1
+            print("Wrong answer! You have", attempts ,"attempts left")
+            count+=1
         else:
             print("You've run out of attempts! try again in a few hours")
     if a==password:
-        print("You have  logged into your account in", счет, "attempts")
+        print("You have  logged into your account in", count, "attempts")
         break
 
 
